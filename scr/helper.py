f2238765a8bc7ba7a0f4446ee14ebb179327eead
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2026-07-15 15:11:29 +0700

    CHORE: Cleaning code and wirte according to pep8

--- scr/helper.py
+++ scr/helper.py
@@ -3,7 +3,9 @@ import sys
 from pathlib import Path
 
 
-if "__file__" in globals() or (sys.argv and ("models.py" in sys.argv[0] or "helper.py" in sys.argv[0])):
+if "__file__" in globals() or (
+    sys.argv and ("models.py" in sys.argv[0] or "helper.py" in sys.argv[0])
+):
     PROJECT_ROOT = Path(__file__).resolve().parent.parent
 else:
     PROJECT_ROOT = Path(os.getcwd()).resolve()
@@ -18,7 +20,7 @@ def resolve_path(path: str) -> Path:
     target_path = PROJECT_ROOT / path
 
     if "models" in target_path.parts:
-        if target_path.suffix not in ['.pt', '.pth'] and not target_path.is_dir():
-            target_path = target_path.with_suffix('.pt')
+        if target_path.suffix not in [".pt", ".pth"] and not target_path.is_dir():
+            target_path = target_path.with_suffix(".pt")
 
     return target_path
